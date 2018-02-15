5acd3870964e63d7642f2c6d78fbe67a63eb351c
Chase Lirette <chaselirette@yahoo.com>
2018-02-15 14:15:52 -0600

    Got a working exploitable app and copied it over to protected_app with the intention of adding prepared statements to fix sql-injection vulnerability.

---
+++ protected_app.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hardware Store</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Katie's Hardware</h1>
+	<form action="" method="GET">
+		<input type="text" name="search">
+		<input type="submit" name="submit" value="Search">
+	</form>
+	<?php
+	if (isset($_GET['search'])) {
+		$host = "localhost";
+		$db = "hardware_store";
+		$user = "root";
+		$pwd = "mysqlt00r";
+		$link = new mysqli($host, $user, $pwd, $db);
+		if ($link->connect_error) {
+			die("Connection failed: " . $link->connect_error);
+		}
+		$item = $_GET['search'];
+		$query = "SELECT * FROM products WHERE name LIKE '%$item%'";
+		$results = mysqli_query($link, $query);
+		// if ($results->num_rows === 0) {
+		// 	echo "<p>No results found</p>"
+		// }
+		while($row = mysqli_fetch_array($results))
+		{
+			echo '<table class="table table-striped table-bordered table-hover">'; 
+			echo "<tr><th>Name</th><th>Description</th><th>Cost</th><th>Availability</th></tr>"; 
+			while($row = mysqli_fetch_array($results))
+			{
+			  echo "<tr><td>"; 
+			  echo $row['name'];
+			  echo "</td><td>";   
+			  echo $row['description'];
+			  echo "</td><td>";
+			  echo $row['price'];
+			  echo "</td><td>";    
+			  echo $row['availability'];
+			  echo "</td></tr>";  
+			}
+			echo "</table>"; 
+		}
+	}	
+	?>
+
+</body>
+</html>
--- test.sql
+++ test.sql
@@ -4,3 +4,4 @@ SELECT ? FROM ? WHERE ? LIKE '%item%'; --example
 SELECT ? FROM ? WHERE ? LIKE '%hammer' AND 0 = SLEEP(2);-- 
 
 SELECT ? FROM ? WHERE ? LIKE '%hammer' UNION (SELECT TABLE_NAME, TABLE_SCHEMA, 3, 4 FROM information_schema.tables);--  
+select `column_name` from `information_schema`.`columns` where `table_schema`='hardware_store' and `table_name`='store_users';
