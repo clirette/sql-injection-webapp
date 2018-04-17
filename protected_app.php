1923a160c665e358ff0560bf0c4d8cba2c6a6f50
Chase Lirette <chaselirette@yahoo.com>
2018-04-17 16:55:24 -0500

    Finished implementation of protected app

--- protected_app.php
+++ protected_app.php
@@ -16,35 +16,34 @@
 		$db = "hardware_store";
 		$user = "root";
 		$pwd = "ch@ng3dt00r";
-		$link = new mysqli($host, $user, $pwd, $db);
-		if ($link->connect_error) {
-			die("Connection failed: " . $link->connect_error);
-		}
-		$item = $_GET['search'];
-		$query = "SELECT * FROM products WHERE name LIKE '%$item%'";
-		$results = mysqli_query($link, $query);
-		// if ($results->num_rows === 0) {
-		// 	echo "<p>No results found</p>"
-		// }
-		while($row = mysqli_fetch_array($results))
-		{
+		try {
+			$conn = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$item = $_GET['search'];
+	
+			$stmt = $conn->prepare("SELECT * FROM products WHERE name LIKE :item");
+			$stmt->bindParam(':item', $item);
+			$stmt->execute();
+			$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			echo '<table class="table table-striped table-bordered table-hover">'; 
 			echo "<tr><th>Name</th><th>Description</th><th>Cost</th><th>Availability</th></tr>"; 
-			while($row = mysqli_fetch_array($results))
-			{
-			  echo "<tr><td>"; 
-			  echo $row['name'];
-			  echo "</td><td>";   
-			  echo $row['description'];
-			  echo "</td><td>";
-			  echo $row['price'];
-			  echo "</td><td>";    
-			  echo $row['availability'];
-			  echo "</td></tr>";  
+			
+			foreach($results as $row) {
+				echo "<tr>";
+				echo "<td>".$row['name']."</td>";
+				echo "<td>".$row['description']."</td>";
+				echo "<td>".$row['price']."</td>";
+				echo "<td>".$row['availability']."</td>";
+				echo "</tr>";
 			}
-			echo "</table>"; 
+			echo "</table>";
+			
+		} catch(PDOException $e) {
+			echo "Error: " . $e->getMessage();
 		}
-	}	
+    	$conn = null;
+    }
 	?>
 
 </body>
